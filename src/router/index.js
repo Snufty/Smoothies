29c30ca57edf5097abd734c01b866e546956fd55
Snufty <56336551+Snufty@users.noreply.github.com>
2020-11-09 13:00:21 +0100

    committ

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,23 @@ import AddSmoothie from '../components/AddSmoothie.vue'
 import Editsmoothie from '../components/EditSmoothie'
 import Signup from '../components/auth/Signup.vue'
 import Login from '../components/auth/Login.vue'
-//import firebase from 'firebase'
+
+import firebase from 'firebase'
+import 'firebase/firestore'
 
 Vue.use(VueRouter)
 
-const routes = [
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
   {
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/AddSmoothie',
@@ -36,12 +44,24 @@ const routes = [
     component: Login
   },
 ]
+});
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+router.beforeEach((to, from, next) =>{
+  // check to see if route requires auth
+  if(to.matched.some(rec => rec.meta.requiresAuth)){
+    // check auth state of user
+    let user = firebase.auth().currentUser
+    if(user){
+      // user signed in proceed to route
+      next()
+    } else {
+      // no user signed in redirect to login
+      next({name: 'Login'})
+    }
+  } else {
+    next()
+  }
+});
 
 export default router
 
